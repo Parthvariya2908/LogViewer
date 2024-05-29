36cca5580477c22dc1479ff12fbeb8045e002085
PARTH <variyaparth84@gmail.com>
2024-05-29 12:49:22 +0530

    change1

--- index.js
+++ index.js
@@ -5,33 +5,22 @@ const fs = require("fs");
 const app = express();
 const port = process.env.PORT || 5500;
 const path = require("path");
+const serveIndex = require("serve-index");
 const host = "127.0.0.1";
 app.use(cors());
 app.use(bodyparser.json());
 
 const { mkdir } = require("node:fs/promises");
 const { join } = require("node:path");
-const { readFile } = require("node:fs/promises");
-const { resolve } = require("node:path");
 
 const logsDirectory =
-  process.env.LOGS_DIRECTORY ||
   "C:\\Users\\Parth\\Desktop\\FOLDER VIEWR ONLY BACKEND\\logs";
 
 const currentpath = join(logsDirectory);
 
-const makeDirectory = async (DirName) => {
-  const projectFolder = join(currentpath, DirName);
-  const dirCreation = await mkdir(projectFolder, { recursive: true });
-  console.log(dirCreation);
-  return dirCreation;
+const makeDirectory = async (pathofdir) => {
+  const dirCreation = await mkdir(pathofdir, { recursive: true });
 };
-// makeDirectory().catch(console.error);
-
-// app.use((req, res, next) => {
-//   console.log(`${req.method} request received for ${req.url}`);
-//   next(); // Call next to pass control to the next middleware function
-// });
 
 const cheakDirectory = (DirName) => {
   const projectFolder = path.join(currentpath, DirName);
@@ -42,95 +31,66 @@ const cheakDirectory = (DirName) => {
   }
 };
 
-const cheakForTheMonth = (folders) => {
+const cheakfolder = (reqpath, DirName) => {
+  const projectFolder = path.join(reqpath, DirName);
+  if (fs.existsSync(projectFolder)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const cheakForTheMonth = () => {
   const currentDate = new Date();
   const year = currentDate.getFullYear();
   const month = currentDate.getMonth() + 1;
   const whole = `${month}-${year}`;
-  const folderExists = folders.some((folder) => folder.name === whole);
-  if (!folderExists) {
-    makeDirectory(whole);
+  const folderpath = join(currentpath, whole);
+  if (!fs.existsSync(folderpath)) {
+    makeDirectory(folderpath);
   }
+  return folderpath;
 };
 
-// const cheakForTheFile = () => {
-//   const currentDate = new Date();
-//   const date = currentDate.getDate();
-//   const year = currentDate.getFullYear();
-//   const month = currentDate.getMonth() + 1;
-//   const whole = `${date}-${month}-${year}`;
-//   if (cheakDirectory(date)) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
-
-app.get("/logs/", async (req, res) => {
-  if (!cheakDirectory("logs")) {
-    const projectFolder = join(currentpath, "logs");
-    const dirCreation = await mkdir(projectFolder, { recursive: true });
-  }
-  const requestedPath = req.query.path || "";
-  const currentDir = path.join(currentpath, requestedPath);
-  fs.readdir(currentDir, { withFileTypes: true }, (err, files) => {
-    if (err) {
-      return res.status(500).json({ error: "Unable to scan directory" });
+app.use((req, res, next) => {
+  const reqpath = req.url;
+  const dirs = reqpath.split("/");
+  if (dirs[1] == "logs") {
+    console.log(dirs);
+    if (dirs[dirs.length - 1].slice(-4) == ".log") {
+      const filepath = join(
+        "C:\\Users\\Parth\\Desktop\\FOLDER VIEWR ONLY BACKEND",
+        reqpath
+      );
+      console.log(filepath);
+      res.sendFile(filepath);
+    } else {
+      next();
     }
-    const folders = files.map((file) => ({
-      name: file.name,
-      isDirectory: file.isDirectory(),
-      path: path.relative(currentpath, path.join(currentDir, file.name)),
-    }));
-    const currentDirName = path.basename(currentDir);
-    console.log(currentDirName);
+  } else {
+    const monthpath = cheakForTheMonth();
+    var pathtodir = join(monthpath, reqpath);
+    if (!cheakfolder(monthpath, reqpath)) {
+      makeDirectory(path.join(monthpath, reqpath));
+    }
     const currentDate = new Date();
     const date = currentDate.getDate();
     const year = currentDate.getFullYear();
     const month = currentDate.getMonth() + 1;
-    const whole = `${month}-${year}`;
-    if (currentDirName === "logs") {
-      console.log("cheaking in logfile");
-      cheakForTheMonth(folders);
-    } else {
-      console.log("cheaking in inner file");
-      if (currentDirName != whole) {
-        const fileName = `${date}-${month}-${year}.log`;
-        const filepath = path.join(currentDir, fileName);
-        const data = `${new Date().toISOString()} - ${req.method} ${req.url}\n`;
-        fs.appendFile(filepath, data, (err) => {
-          if (err) {
-            console.error("Error writing to log file:", err);
-          }
-        });
-        const folderExists = folders.some((folder) => folder.name === fileName);
-        if (!folderExists) {
-          folders.push({
-            name: fileName,
-            isDirectory: false,
-            path: path.relative(currentpath, path.join(currentDir, fileName)),
-          });
-        }
-
-        console.log(folders);
+    const whole = `${date}-${month}-${year}.log`;
+    const data = `${new Date().toISOString()} - ${req.method} ${
+      req.headers["user-agent"]
+    }\n`;
+    fs.appendFile(join(pathtodir, whole), data, (err) => {
+      if (err) {
+        console.error("Error writing to log file:", err);
       }
-    }
-    res.json({ folders });
-  });
+    });
+    next();
+  }
 });
 
-app.get("/file/", (req, res) => {
-  const requestedPath = req.query.path || "";
-  const currentDir = path.join(currentpath, requestedPath);
-
-  res.sendFile(currentDir, (err) => {
-    if (err) {
-      console.error("Error sending file:", err);
-      res.status(500).send("Error sending file");
-    }
-  });
-});
-
-app.listen(port, host, () => {
+app.use("/logs", serveIndex(currentpath, { icons: true }));
+app.listen(port, () => {
   console.log("LISTINING ON PORT", 5500);
 });
